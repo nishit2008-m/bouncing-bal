e3007b83021b340ecf5f89e1018b848bf0ce0c87
nishit2008-m <84621798+nishit2008-m@users.noreply.github.com>
2021-08-23 16:59:03 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,58 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+
+let engine;
+let world;
+
+var ground;
+var left;
+var right;
+var top_wall;
+
+function setup() {
+  createCanvas(400,400);
+  engine = Engine.create();
+  world = engine.world;
+  button1=createImg("push.png")
+  button1.position(20,30)
+  button1.size(60,60)
+button1.mouseClicked(vforce)
+
+  button2=createImg("right.png")
+  button2.position(220,30)
+  button2.size(60,60)
+  button2.mouseClicked(hforce)
+
+  ground =new Ground(200,390,400,20);
+  right = new Ground(390,200,20,400);
+  left = new Ground(10,200,20,400);
+  top_wall = new Ground(200,10,400,20);
+ var ball_options={
+   restitution:0.85
+ }
+ ball=Bodies.circle(200,10,20,ball_options)
+ World.add(world,ball)
+  rectMode(CENTER);
+  ellipseMode(RADIUS);
+}
+
+function draw() 
+{
+  background(51);
+  ground.show();
+  top_wall.show();
+  left.show();
+  right.show();
+  ellipse(ball.position.x,ball.position.y,20,20)
+  Engine.update(engine);
+}
+
+function hforce(){
+  Matter.Body.applyForce(ball,{x:0,y:0},{x:0.05,y:0});
+  
+}
+function vforce(){
+  Matter.Body.applyForce(ball,{x:0,y:0},{x:0,y:-0.05});
+}
+
